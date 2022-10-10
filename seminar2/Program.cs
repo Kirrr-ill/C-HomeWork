0dbd3f09c9c23edf7ce2b90765059495af14f32e
Kirill <mail@kirrr-ill.ru>
2022-10-10 18:34:56 +0300

    Homework done3

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,53 +1,53 @@
 ﻿
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// Console.Write("Введите трёхзначное число: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// if(n > = 100 && n < 1000)
-// {
-//     n = n % 100;
-//     n = n / 10;
+if(n > = 100 && n < 1000)
+{
+    n = n % 100;
+    n = n / 10;
 
-//     Console.WriteLine("Вторая цифра: " + n);
-// }
-// else
-// {
-//     Console.WriteLine("Три цифры, пожалуста!");
-// }
+    Console.WriteLine("Вторая цифра: " + n);
+}
+else
+{
+    Console.WriteLine("Три цифры, пожалуста!");
+}
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// string thirdNumber = Convert.ToString(number);
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string thirdNum = Convert.ToString(num);
 
-// if (thirdNumber.Length > 2)
-// {
-//     Console.WriteLine("Третья цифра: " + thirdNumber[2]);
-// }
-// else 
-// {
-//     Console.WriteLine("Третьей цифры нет.");
-// }
+if (thirdNum.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + thirdNum[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
 
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// Console.Write("Введите номер дня недели: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// if(n == 6 || n == 7)
-// {
-//     Console.WriteLine("ВЫХОДНОЙ!!!");
-// }
-// if (n < 1 || n > 7) 
-// {
-//     Console.WriteLine("В неделе семь дней!");
-// }
-// else
-// {
-//     Console.WriteLine("Будний день.");
-// }
+if(n == 6 || n == 7)
+{
+    Console.WriteLine("ВЫХОДНОЙ!!!");
+}
+if (n < 1 || n > 7) 
+{
+    Console.WriteLine("В неделе семь дней!");
+}
+else
+{
+    Console.WriteLine("Будний день.");
+}
