7dc349456b9dfdd07bd7da60a4abb13de755bca6
Kirill <mail@kirrr-ill.ru>
2022-10-10 13:00:07 +0300

    Homework done

---
+++ seminar2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("Введите трёхзначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if(n > = 100 && n < 1000)
+// {
+//     n = n % 100;
+//     n = n / 10;
+
+//     Console.WriteLine("Вторая цифра: " + n);
+// }
+// else
+// {
+//     Console.WriteLine("Три цифры, пожалуста!");
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string thirdNumber = Convert.ToString(number);
+
+// if (thirdNumber.Length > 2)
+// {
+//     Console.WriteLine("Третья цифра: " + thirdNumber[2]);
+// }
+// else 
+// {
+//     Console.WriteLine("Третьей цифры нет.");
+// }
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("Введите номер дня недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if(n == 6 || n == 7)
+//     Console.WriteLine("ВЫХОДНОЙ!!!");
+
+// else
+//     Console.WriteLine("Будний день.");
